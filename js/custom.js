81c0f898b093cbee853c0311169955bb7b2aae7d
Nishe Rahman <91620950+nisherahman@users.noreply.github.com>
2022-04-13 22:27:29 +0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,32 @@
 $(document).ready(function(){
 
+    // scroll position
+    $(window).on('scroll',function(){
+    var scrolling = $(this).scrollTop();
 
+    // back to top appearance 
+    if(scrolling > 100){
+      $('.back-to-top').fadeIn();
+    }else{
+      $('.back-to-top').fadeOut();  
+    }
+    // navbar class 
+    if(scrolling > 150){
+      $('nav').addClass('nav-fix');
+    }else{
+      $('nav').removeClass('nav-fix');
+    }              
+
+
+    });
+
+    // back to top action 
+    $('.back-to-top').on('click',function(){
+      $('html,body').animate({
+      scrollTop: '0px',
+    },2000);
+  
+    });
 
     //product slider 
     $('.product-image-slider').slick({
@@ -36,6 +62,33 @@ $(document).ready(function(){
         centerPadding:'0px',
         prevArrow:'<i class="fas fa-chevron-left slick-prev prev-arrow"></i>',
         nextArrow:'<i class="fas fa-chevron-right slick-next next-arrow"></i>',
+        responsive:[
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+              arrows:true,
+            }
+        },
+          {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                arrows:true,
+              }
+          },
+          {
+              breakpoint: 576,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows:false,
+              }
+          },
+  
+        ],
     });
   
     //blog pop up
